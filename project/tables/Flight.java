41effbb10ecfe87df4e31760f5d812003352f57a
Joel Fuentes <joel.fuentes@uci.edu>
2015-05-09 11:16:57 -0700

    Basic schemme for table-rows classes

---
+++ project/tables/Flight.java
@@ -0,0 +1,54 @@
+
+//implementation of a Tuple for Flights table
+
+public class Flight{
+  //attributes
+  private String flightNum; //primary key
+  private int price;
+  private int numSeats;
+  private int numAvail;
+  //add more attributes needed to guarantee ACID
+
+  //constructor
+  public Flight(String flightNum, int price, int numSeats, int numAvail){
+    this.flightNum=flightNum;
+    this.price=price
+    this.numSeats=numSeats;
+    this.numAvail=numAvail;
+  }
+
+
+  //methods
+  public String getFlightNum(){
+    return flightNum;
+  }
+
+  public void setFlightNum(String flightNum){
+    this.flightNum=flightNum;
+  }
+
+  public int getPrice(){
+    return price;
+  }
+
+  public void setPrice(int price){
+    this.price=price;
+  }
+
+  public int getNumSeats(){
+    return numSeats;
+  }
+
+  public void setNumSeats(int numSeats){
+    this.numSeats=numSeats;
+  }
+
+  public int getNumAvail(){
+    return numAvail;
+  }
+
+  public void setNumAvail(int numAvail){
+    this.numAvail=numAvail;
+  }
+
+}
---
+++ project/tables/Flights.java
@@ -0,0 +1,19 @@
+//implementation of the Flights table
+
+public class Flights{
+  //attributes
+  private Map<String, Flight> table;
+  //perhaps we will add more attributes to guarantee ACID
+
+
+  //constructor
+  public Flights(){
+    table= new HashMap<String, Flight>();
+  }
+
+  //methods
+
+  /**
+  * Methods to add, update, remove and search tuples on the table
+  **/
+}
